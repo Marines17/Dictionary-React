311e374a52ea3ebf9697dbb964759258e65b4502
Marines17 <simon.marine33@gmail.com>
2021-07-15 10:24:29 +0100

    updated results file css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3 className="text-capitalize ">{props.meaning.partOfSpeech}</h3>
+      <h3 className="text-capitalize partOfSpeech">
+        {props.meaning.partOfSpeech}
+      </h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>

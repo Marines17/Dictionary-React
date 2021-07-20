b65c7769507b1ff427fe6ea8ff1b6faaf96a2598
Marines17 <simon.marine33@gmail.com>
2021-07-20 09:12:19 +0100

    Changed color and font size meaning and synonym component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,7 @@ export default function Synonyms(props) {
     return (
       <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}, </span>;
+          return <span key={index}>{synonym} </span>;
         })}
       </div>
     );
